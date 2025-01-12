790f257964cbaa88c083b2b8085dbba930ad9c17
orangess <forwow589@gmail.com>
2025-01-12 21:48:35 +0300

    added the count_words method

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     text = get_book_text("books/frankenstein.txt")
     print(text)
+    print(count_words(text))
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+def count_words(text):
+    words = []
+    words = text.split()
+    return len(words)
 main()
